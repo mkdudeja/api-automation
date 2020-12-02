9f9b12eb23637968adc65454764b77274574e343
Manish Kumar <Kumar.Manish02@bcg.com>
2020-12-02 21:24:56 +0530

    Chrome extension base setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var chrome: any;
+const RECORDS_LIMIT = 400;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'APIAutomation';
+
+  port = null;
+  calls = [];
+  callsData = {};
+  tabs = {};
+
+  ngOnInit() {
+    this.port = chrome.runtime.connect({ name: 'start_listen' });
+    this.port.onMessage.addListener(this._onMessage.bind(this));
+  }
+
+  private _onMessage(msg: any) {
+    const callId = msg['req_details']['requestId'];
+    const callUrl = msg['req_details']['url'];
+
+    if (!this.callsData[callId]) {
+      this.callsData[callId] = { url: callUrl };
+      this.calls.push(callId);
+
+      if (this.calls.length > RECORDS_LIMIT) {
+        const lastCall = this.calls.shift();
+        this.callsData[lastCall]['tr_obj'].remove();
+        this.callsData[lastCall]['tr_details'].remove();
+        delete this.callsData[lastCall];
+      }
+    }
+
+    this.callsData[callId][msg['type']] = msg['req_details'];
+
+    console.log(this.callsData);
+  }
 }
