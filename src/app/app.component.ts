4fbba190fa7cb7202da6fed4774c7179cc920996
Annadurai, Bagavathi Murugan <Annadurai.BagavathiMurugan@bcg.com>
2020-12-03 21:34:03 +0530

    Integration with template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
 import { HARDataSource } from './HAR_Data';
+import { getMethodTemplate, postMethodTemplate, testClassTemplate} from './test-template'
 
 interface IRequest extends chrome.devtools.network.Request {
   _resourceType: string;
@@ -55,12 +56,11 @@ export class AppComponent implements OnInit {
   }
 
   public generateScript(
-    request: IRequest,
-    data: string = 'hello world...',
-    filename: string = `${new Date().toDateString()}.txt`
+    request: IRequest
   ) {
-    const blob = new Blob([data], { type: 'application/octet-stream' });
-    chrome.downloads.download({ url: URL.createObjectURL(blob), filename });
+    // const blob = new Blob([data], { type: 'application/octet-stream' });
+    // chrome.downloads.download({ url: URL.createObjectURL(blob), filename });
+    this._generateScripts([request]);
   }
 
   public generateSelectedScript() {
@@ -68,9 +68,59 @@ export class AppComponent implements OnInit {
       (apiRequest) => apiRequest.selected
     );
     console.log(selectedApis);
+    this._generateScripts(selectedApis);
   }
 
   public generateAllScript() {
+    this._generateScripts(this.apiRequests);
     console.log(this.apiRequests);
+  }
+
+  private _generateScripts(requests: IRequest[]) {
+    let generatedMethods = '';
+    let baseUrl = '';
+    for(let i = 0; i < requests.length; i++) {
+      const apiRequest = requests[i];
+
+      let method : string;
+      if (apiRequest.request.method === 'GET') {
+        method = getMethodTemplate;
+      } else {
+        method = postMethodTemplate;
+      }
+
+      if (!baseUrl && apiRequest.request.url.indexOf('/api') > 0) {
+        baseUrl = apiRequest.request.url.split('/api')[0] + '/';
+      }
+
+      method = method.replace('[[Order]]', (i+1).toString());
+      const urlParts = apiRequest.request.url.split('/'); // exclude query param?
+
+      let testName = urlParts[urlParts.length - 1];
+      if (apiRequest.request.url.indexOf('?') >= 0) {
+        testName = testName.split('?')[0];
+      }
+
+      method = method.replace('[[TestName]]', testName);
+
+      let content = apiRequest.request.postData ? apiRequest.request.postData.text : '';
+      content = content.replace(/\"/g, '""');
+      method = method.replace('[[JSONRequestContent]]', content);
+
+      content = apiRequest.response.content ? apiRequest.response.content.text : '';
+      content = content.replace(/\"/g, '""');
+      method = method.replace('[[JSONResponseContent]]', content);
+
+      method = method.replace('[[ApiUrl]]', baseUrl ? apiRequest.request.url.replace(baseUrl, ''): apiRequest.request.url);
+
+      generatedMethods += method;
+    }
+
+    let testClass = testClassTemplate.replace('[[HostUrl]]', baseUrl);
+
+    testClass = testClass.replace('[[TEST_CASES]]', generatedMethods);
+
+    const blob = new Blob([testClass], { type: 'application/octet-stream' });
+    chrome.downloads.download({ url: URL.createObjectURL(blob), filename: 'AutoApiTestClass.cs' });
   }
 }
