12d37ebbbf7c1ba127850d418dde9e3b6d7db29c
Manish Kumar <87manishkumar@gmail.com>
2020-12-03 11:48:49 +0530

    added HAR sample data source to get started with app

---
+++ src/devtoolsBackground.js
@@ -0,0 +1,6 @@
+var panels = chrome && chrome.devtools && chrome.devtools.panels;
+var elementsPanel = panels && panels.elements;
+
+if (panels) {
+  panels.create("API Automation", "wifi_network.png", "index.html");
+}
