1a42185bbee17dc19f8fd0f45dfa07ccedf524d3
Kempegowda, Pratheek <Kempegowda.Pratheek@bcg.com>
2020-12-03 14:28:43 +0530

    UI table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HARDataSource } from './HAR_Data';
 
 interface IRequest extends chrome.devtools.network.Request {
   _resourceType: string;
@@ -12,18 +13,25 @@ interface IRequest extends chrome.devtools.network.Request {
 export class AppComponent implements OnInit {
   title = 'APIAutomation';
   dataSource: Array<IRequest> = [];
+  apiRequests = HARDataSource.filter(apiRequest => apiRequest._resourceType === 'xhr');
 
   ngOnInit() {
-    chrome.devtools.network.onRequestFinished.addListener(
-      (request: IRequest) => {
-        if (request._resourceType === 'xhr') {
-          request.getContent((content: string, encoding: string) => {
-            request.response.content.text = content;
-            this.dataSource.push(request);
-            console.log('this.dataSource', this.dataSource);
-          });
-        }
-      }
-    );
+    // chrome.devtools.network.onRequestFinished.addListener(
+    //   (request: IRequest) => {
+    //     if (request._resourceType === 'xhr') {
+    //       request.getContent((content: string, encoding: string) => {
+    //         request.response.content.text = content;
+    //         this.dataSource.push(request);
+    //         console.log('this.dataSource', this.dataSource);
+    //       });
+    //     }
+    //   }
+    // );
+    console.log(this.apiRequests);
+
+  }
+
+  public generateScript(request) {
+    console.log(request);
   }
 }
